be7ca64ce805a86c9a79f6bf6599511eb9ff6c01
Thiago Kaique Ribeiro Fonseca <66787043+Thiago099@users.noreply.github.com>
2020-12-14 19:54:05 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace feedback_in_bruteforce_app_creation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = 26;//number of letters
+            var m = 4;//max number of digits
+            for (int j = 1; j < m + 1; j++)
+                for (int i = 0; i < Math.Pow(p, j); i++)
+                {
+                    for (int k = 1; k < j + 1; k++) //Console.WriteLine();
+                        Console.Write((char)((i / Math.Pow(p, k - 1) % p) + 'a'));
+                   
+                    //Console.Write((char)((i % 2 )+'a'));
+
+
+                    Console.Write(" | ");
+                    for (int k = j; k > 0; k--) Console.Write(Math.Pow(p, k - 1) + " "); Console.Write(" | ");
+
+                    Console.Write(i % p + "\t");
+                    Console.Write(i / p + "\t");
+
+                    Console.Write(" | ");for (int k = 1; k < j + 1; k++) Console.Write((int)((i / Math.Pow(p, k - 1) % p))+" ");
+
+
+                    Console.WriteLine();
+
+
+                }
+
+            
+            Console.ReadKey();
+        }
+    }
+}
